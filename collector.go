2b8630904380cc2b5be7d7608c4c54e9d4c5dd25
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2018-04-20 10:46:25 +0300

    collector

---
+++ collector.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"github.com/go-redis/redis"
+	"time"
+	"log"
+	"fmt"
+	"io/ioutil"
+)
+
+
+func writetoRedis(rediskey string , redisVal string){
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})	
+	fmt.Println(redisVal)
+
+	llength, _ := client.LLen(rediskey).Result()
+    fmt.Println(llength)
+	if llength == 100 {
+		client.LPop(rediskey)
+	}
+	client.RPush(rediskey, redisVal)
+}
+
+
+func main() {
+	user := os.Args[1]
+	host := os.Args[2]
+	netService := os.Args[3]
+	app := os.Args[4]
+	time.Sleep(time.Second*5)
+	for {
+		collect(user, host, netService, app)
+		time.Sleep(time.Second*1)
+	}
+}
+
+func collect(user, host, service, app string) {
+	url := "http://167.99.213.33:9000/v1/data"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	responsedata, errori := ioutil.ReadAll(resp.Body)
+	if errori != nil {
+	   log.Fatal(err)
+	}
+	rediskey := fmt.Sprintf("heatmaps:%s:%s:%s:%s:latency", user, host, service, app)
+    //b, erro := json.Marshal(resp)
+	fmt.Println(string(responsedata))
+	writetoRedis(rediskey, string(responsedata))
+
+}
+
