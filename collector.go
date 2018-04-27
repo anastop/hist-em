fdedb0fcbb0addcb815d5242268298607d06bd7a
Chris Mark <chrismarkou92@gmail.com>
2018-04-27 12:14:43 +0000

    Fixed stylings and minor types

--- collector.go
+++ collector.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-redis/redis"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"github.com/go-redis/redis"
 	"time"
-	"log"
-	"fmt"
-        "encoding/json"
-	"io/ioutil"
 )
 
-
-func writetoRedis(rediskey string , redisVal string){
+func writetoRedis(rediskey string, redisVal string) {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
@@ -21,52 +20,49 @@ func writetoRedis(rediskey string , redisVal string){
 	fmt.Println(redisVal)
 
 	llength, _ := client.LLen(rediskey).Result()
-    fmt.Println(llength)
+	fmt.Println(llength)
 	if llength == 100 {
 		client.LPop(rediskey)
 	}
 	client.RPush(rediskey, redisVal)
 }
 
-
 func main() {
 	user := os.Args[1]
 	host := os.Args[2]
 	netService := os.Args[3]
 	app := os.Args[4]
-	time.Sleep(time.Second*5)
+	time.Sleep(time.Second * 5)
 	for {
 		collect(user, host, netService, app)
-		time.Sleep(time.Second*1)
+		time.Sleep(time.Second * 1)
 	}
 }
 
-
 type CollectorData struct {
-  Timestamp int64        `json:"timestamp"`
-  Data      json.RawMessage  `json:"data"`
+	Timestamp int64           `json:"timestamp"`
+	Data      json.RawMessage `json:"data"`
 }
 
 func collect(user, host, service, app string) {
 	url := "http://167.99.213.33:9000/v1/data"
 	resp, err := http.Get(url)
 	if err != nil {
-          log.Fatal(err)
+		log.Fatal(err)
 	}
 	responsedata, errori := ioutil.ReadAll(resp.Body)
 	if errori != nil {
-	  log.Fatal(errori)
+		log.Fatal(errori)
 	}
 	rediskey := fmt.Sprintf("heatmaps:%s:%s:%s:%s:latency", user, host, service, app)
-    //b, erro := json.Marshal(resp)
+	//b, erro := json.Marshal(resp)
 	fmt.Println(string(responsedata))
 
-        m := CollectorData{Data: responsedata, Timestamp: time.Now().Unix()}
-        data, errorios := json.Marshal(m)
-        if errorios != nil {
-          log.Fatal(errorios)
-        }
-        writetoRedis(rediskey, string(data))
+	m := CollectorData{Data: responsedata, Timestamp: time.Now().Unix()}
+	data, errorios := json.Marshal(m)
+	if errorios != nil {
+		log.Fatal(errorios)
+	}
+	writetoRedis(rediskey, string(data))
 
 }
-
--- server.go
+++ server.go
@@ -1,135 +1,138 @@
 package main
 
 import (
-  "net/http"
-  "strings"
-  "encoding/json"
-  "github.com/cleversoap/go-cp"
-  "os/exec"
-  "log"
-  "fmt"
-  "bufio"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/cleversoap/go-cp"
+	"log"
+	"net/http"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 var quit chan struct{}
 
 type histogramColumn struct {
-    Value  string `json:"value"`
-    Count  string `json:"count"`
+	Value float64 `json:"value"`
+	Count int64   `json:"count"`
 }
 
 type histogram struct {
-    Histogram []histogramColumn `json:"histogram"`
+	Histogram []histogramColumn `json:"histogram"`
 }
 
-
-
-
-func run_cyclictest() *exec.Cmd{
-  cmd := exec.Command("sh", "-c", "./cyclictest -q -D2 -h100 > outputGo")
-  err := cmd.Start()
-  if err != nil {
-    log.Fatal(err)
-  }
-  return cmd
+func runCyclictest() *exec.Cmd {
+	cmd := exec.Command("sh", "-c", "./cyclictest -q -D2 -h100 > outputGo")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return cmd
 }
 
-func start_cyclictest() {
-  var cmd *exec.Cmd
-  for {
-    select {
-    case <-quit:
-      // kill_cyclictest(cmd)
-      break
-    default:
-      cmd = run_cyclictest()
-      cmd.Wait()
-      err := cp.Copy("outputGo", "output.txt")
-      if err != nil {
-        panic(err)
-      }
-    }
-  }
+func startCyclictest() {
+	var cmd *exec.Cmd
+	for {
+		select {
+		case <-quit:
+			// kill_cyclictest(cmd)
+			break
+		default:
+			cmd = runCyclictest()
+			cmd.Wait()
+			err := cp.Copy("outputGo", "output.txt")
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
 
 }
 
-func kill_cyclictest(cmd *exec.Cmd){
-  // Kill it:
-  if err := cmd.Process.Kill(); err != nil {
-    log.Fatal("failed to kill process: ", err)
-  }
+func killCyclictest(cmd *exec.Cmd) {
+	// Kill it:
+	if err := cmd.Process.Kill(); err != nil {
+		log.Fatal("failed to kill process: ", err)
+	}
 }
 
+func parseCyclictestResults() histogram {
+	var val float64
+	var count int64
+	var errori error
+	cmd := exec.Command("sh", "-c", "python ./create_hist.py")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func parse_cyclictest_results() histogram{
-  cmd := exec.Command("sh", "-c", "python ./create_hist.py")
-  stdout, err := cmd.StdoutPipe()
-  if err != nil {
-    log.Fatal(err)
-  }
+	in := bufio.NewScanner(stdout)
 
-  in := bufio.NewScanner(stdout)
-
-  err = cmd.Start()
-  if err != nil {
-    log.Fatal(err)
-  }
-  var  histArray histogram
-  var histArr []histogramColumn
-  for in.Scan(){
-    line := in.Text()
-    fmt.Println(line)
-    s := strings.Split(line, " ")
-    histArr = append(histArr, histogramColumn{Value: s[0], Count: s[1]})
-  }
-  histArray.Histogram = histArr
-  fmt.Println()
-  return histArray
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var histArray histogram
+	var histArr []histogramColumn
+	for in.Scan() {
+		line := in.Text()
+		fmt.Println(line)
+		s := strings.Split(line, " ")
+		if val, errori = strconv.ParseFloat(s[0], 64); errori != nil {
+			log.Fatal(errori)
+		}
+		if count, errori = strconv.ParseInt(s[1], 10, 64); errori != nil {
+			log.Fatal(errori)
+		}
+		col := histogramColumn{
+			Value: val,
+			Count: count,
+		}
+		histArr = append(histArr, col)
+	}
+	histArray.Histogram = histArr
+	fmt.Println()
+	return histArray
 }
 
-func stopCyclRoutine(){
-  quit <- struct{}{}
+func stopCyclRoutine() {
+	quit <- struct{}{}
 }
-
-
 
 func HTTPDataHandler(w http.ResponseWriter, r *http.Request) {
-  ret := parse_cyclictest_results()
-  js, err := json.Marshal(ret)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
+	ret := parseCyclictestResults()
+	js, err := json.Marshal(ret)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  w.Header().Set("Content-Type", "application/json")
-  w.Write(js)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 
 }
 
 func HTTPStopHandler(w http.ResponseWriter, r *http.Request) {
-   stopCyclRoutine()
-   w.Header().Set("Server", "Cyclic Web Server")
-   w.WriteHeader(200)
+	stopCyclRoutine()
+	w.Header().Set("Server", "Cyclic Web Server")
+	w.WriteHeader(200)
 }
-
 
 func HTTPStartHandler(w http.ResponseWriter, r *http.Request) {
 
-   quit = make(chan struct{})
-   go start_cyclictest()
-   w.Header().Set("Server", "Cyclic Web Server")
-   w.WriteHeader(200)
+	quit = make(chan struct{})
+	go startCyclictest()
+	w.Header().Set("Server", "Cyclic Web Server")
+	w.WriteHeader(200)
 
 }
-
 
 func main() {
 
-   http.HandleFunc("/v1/data", HTTPDataHandler)
-   http.HandleFunc("/v1/start", HTTPStartHandler)
-   http.HandleFunc("/v1/stop", HTTPStopHandler)
-   log.Fatal(http.ListenAndServe(":9000", nil))
+	http.HandleFunc("/v1/data", HTTPDataHandler)
+	http.HandleFunc("/v1/start", HTTPStartHandler)
+	http.HandleFunc("/v1/stop", HTTPStopHandler)
+	log.Fatal(http.ListenAndServe(":9000", nil))
 }
-
-
-
